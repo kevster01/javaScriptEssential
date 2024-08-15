dd101b7492a0286de78d5e2c12769aec80013391
Kevin <kevidarkar12@gmail.com>
2024-08-15 00:00:52 -0400

    added two additional arrow functions to showcase different items and respective prices

--- Array_Itiration/restaurant_menu.js
+++ Array_Itiration/restaurant_menu.js
@@ -1,16 +1,27 @@
-const breakfastMenu = [`Pancakes`, `Eggs Benedict`, `Oatmeal`, `Frittata`];
-const mainCouseMenu = [`Steak`, `Pasta`, `Burger`, `Salmon`]; 
-const dessertMenu = [`Cake`, `Icre Cream`, `Pudding`, `Fruit Salar`]; 
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = [`Steak- $35`, `Pasta- $25`, `Burger- $15`, `Salmon- $45`]; 
+const dessertMenu = [`Cake- $7 `, `Icre Cream- $5`, `Pudding- $5`, `Fruit Salad- $8`]; 
+
+
+
+
 
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
- let mainCourseItem = '';
-mainCourseMenu.forEach((item, index) => {
-mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
-document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+ const mainCourseMenuItemsHTML = mainCourseMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+        document.getElementById('mainCourseMenuItems').innerHTML = mainCourseMenuItemsHTML;
 
-let dessertItem = '';
-for (let i = 0; i < dessertMenu.length; i++) {
-    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
-document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+ const dessertMenuItemsHTML = dessertMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+        document.getElementById('dessertMenuItems').innerHTML = dessertMenuItemsHTML;
+
+
+//  let mainCourseItem = '';
+// mainCourseMenu.forEach((item, index) => {
+// mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+// document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+// let dessertItem = '';
+// for (let i = 0; i < dessertMenu.length; i++) {
+//     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+// document.getElementById('dessertMenuItems').innerHTML = dessertItem;
